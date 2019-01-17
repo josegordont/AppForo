53d7eb599fb3cf3f7efb52925d098313d86d3de2
José Gordón <pepe.jpgt@gmail.com>
2019-01-16 22:19:12 -0500

    componente welcome para ingresar usuario y url de imagen

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
+
+
 export class WelcomeComponent implements OnInit {
 
-  constructor() { }
+  userName: string;
+  userImageUrl: string;
+
+  constructor(private router: Router) { 
+    // inicialización de variables
+    this.userName = '';
+    this.userImageUrl = '';
+  }
 
   ngOnInit() {
+  }
+
+  entryForum(){
+    // Valor por defecto userName
+    if(this.userName.trim() == ''){
+      this.userName = 'Guest';
+    }
+    // Valor por defecto userImageUrl
+    if(this.userImageUrl.trim() == ''){
+        this.userImageUrl = 'https://png.icons8.com/search/96';
+    }
+    this.router.navigateByUrl('/foro/' + btoa(this.userName).replace('=','<') + '/' + btoa(this.userImageUrl).replace('=','<'));
   }
 
 }
